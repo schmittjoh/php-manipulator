68f678308649498c6fe3d42e3a627dd51b89c989
Márcio Almada <marcio3w@gmail.com>
2014-05-11 21:54:02 -0300

    just psr fix, please squash this commit [skip-ci]

--- src/JMS/PhpManipulator/TokenStream/AbstractToken.php
+++ src/JMS/PhpManipulator/TokenStream/AbstractToken.php
@@ -214,6 +214,7 @@ abstract class AbstractToken
     public function getIndentation()
     {
         $indentation = $this->getLineIndentation();
+
         return $indentation . str_repeat(' ', abs($this->getStartColumn() - strlen($indentation)));
     }
 
